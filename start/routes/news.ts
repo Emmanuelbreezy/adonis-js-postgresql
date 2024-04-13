9e61b07c98c240f71b45a659b990b3b44183fe15
Emmanuelbreezy <emmabreezyumeh@gmail.com>
2024-04-13 20:48:39 +0100

    🏷️ updated the news route with controller

--- start/routes/news.ts
+++ start/routes/news.ts
@@ -1,14 +1,9 @@
 import Route from '@ioc:Adonis/Core/Route'
-import Database from "@ioc:Adonis/Lucid/Database";
-
 
 
 export default () => {
     Route.group(() => {
-        Route.get("/", async ({ view }) => {
-            const articles = await Database.from("articles").select('*');
-            return view.render("news.view", {articles});
-          }).as("news_view");
+        Route.get("/", "ArticlesController.view").as("news_view");
 
           Route.patch("/:id", ({ params }) => {
             return { params };
